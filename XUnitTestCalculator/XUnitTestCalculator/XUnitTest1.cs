f08777d5c3393aa728cf56c9e50a2ead061e239c
rahib <rsayed@spartaglobal.com>
2019-02-20 08:38:57 +0000

    incomplete test

---
+++ XUnitTestCalculator/XUnitTestCalculator/XUnitTest1.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+
+namespace XUnitTestCalculator
+{
+    public class XUnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(0, Subtract(2, 2));
+            Assert.Equal(1, Divide(2, 2));
+            Assert.Equal(6, Multiply(3, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(5, Subtract(2, 2));
+            Assert.Equal(5, Divide(2, 2));
+            Assert.Equal(6, Multiply(3, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+
+        int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+}
